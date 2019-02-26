f510edfe9f4bcca9c746eff3caad9ed01967a655
Thomas Beirne <thomas@buildingconnected.com>
2019-02-26 15:42:13 -0800

    Simple useState example

--- index.jsx
+++ index.jsx
@@ -1,23 +1,17 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 import { Root } from './root'
 
-class Counter extends React.Component {
-	state = { count: 0 }
-	render() {
-		return (
-			<Root>
-				<button onClick={() => this.setState(s => ({ count: s.count + 1 }))}>
-					+
-				</button>
-				<button onClick={() => this.setState(s => ({ count: s.count - 1 }))}>
-					-
-				</button>
-				<span>{this.state.count}</span>
-			</Root>
-		)
-	}
+function Counter() {
+	const [count, setCount] = useState(0)
+	return (
+		<Root>
+			<button onClick={() => setCount(count + 1)}>+</button>
+			<button onClick={() => setCount(count - 1)}>-</button>
+			<span>{count}</span>
+		</Root>
+	)
 }
 
 ReactDOM.render(<Counter />, document.querySelector('#app'))
