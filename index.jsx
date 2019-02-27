f84edd0bbae870a23f7086c9de2383cbbf167a9d
Thomas Beirne <thomas@buildingconnected.com>
2019-02-26 19:14:52 -0800

    Demo how context and hooks can work together

--- index.jsx
+++ index.jsx
@@ -1,15 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 import ReactDOM from 'react-dom'
 
-function IntervalExample() {
-	const [timeElapsed, setTimeElapsed] = useState(0)
-	useEffect(() => {
-		setInterval(() => {
-			setTimeElapsed(timeElapsed + 1)
-		}, 1000)
-	}, [])
-
-	return <div>Time elapsed in seconds: {timeElapsed}</div>
+const localeCtx = React.createContext()
+function LocaleProvider({ children }) {
+	const [locale, setLocale] = useState('english')
+	return (
+		<localeCtx.Provider value={{ locale, setLocale }}>
+			{children}
+		</localeCtx.Provider>
+	)
 }
 
-ReactDOM.render(<IntervalExample />, document.querySelector('#app'))
+function LocalePicker() {
+	const { locale, setLocale } = useContext(localeCtx)
+
+	return (
+		<select value={locale} onChange={e => setLocale(e.target.value)}>
+			<option value="english">English</option>
+			<option value="spanish">Spanish</option>
+		</select>
+	)
+}
+
+function Hello() {
+	const { locale } = useContext(localeCtx)
+	switch (locale) {
+		case 'english':
+			return <b>Hello</b>
+		case 'spanish':
+			return <i>Hola</i>
+	}
+}
+
+function App() {
+	return (
+		<LocaleProvider>
+			<LocalePicker />
+			<Hello />
+		</LocaleProvider>
+	)
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
