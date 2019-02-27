d1fae9fb58abe406706403c7b11315f7ed85a11e
Thomas Beirne <thomas@buildingconnected.com>
2019-02-26 16:59:22 -0800

    Introduce a custom hook to handle canceling old promises

--- index.jsx
+++ index.jsx
@@ -9,14 +9,7 @@ function OnlineUsers() {
 	const [users, setUsers] = useState()
 	const [selectedUserId, setSelectedUserId] = useState()
 
-	useEffect(() => {
-		const inFlightPromise = getOnlineUsers().then(u => setUsers(u))
-		return () => {
-			if (inFlightPromise) {
-				inFlightPromise.cancel()
-			}
-		}
-	}, [])
+	useEffectWithCancel(() => getOnlineUsers().then(u => setUsers(u)), [])
 
 	return users === undefined ? (
 		'Loading...'
@@ -36,21 +29,30 @@ function OnlineUsers() {
 
 function User({ id, name }) {
 	const [userDetail, setUserDetail] = useState()
-	useEffect(() => {
+	useEffectWithCancel(() => {
 		setUserDetail(undefined)
-		const inFlightPromise = getUserDetail(id).then(ud => setUserDetail(ud))
-		return () => {
-			if (inFlightPromise) {
-				inFlightPromise.cancel()
-			}
-		}
+		return getUserDetail(id).then(ud => setUserDetail(ud))
 	}, [id])
+
 	return (
 		<div>
 			<h2>{name}</h2>
 			<p>{userDetail ? userDetail.summary : 'Loading...'}</p>
 		</div>
 	)
+}
+
+function useEffectWithCancel(createEffect, deps) {
+	useEffect(() => {
+		let inFlightPromise = createEffect().then(() => {
+			inFlightPromise = null
+		})
+		return () => {
+			if (inFlightPromise) {
+				inFlightPromise.cancel()
+			}
+		}
+	}, deps)
 }
 
 ReactDOM.render(<OnlineUsers />, document.querySelector('#app'))
