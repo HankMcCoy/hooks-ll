ce79b32b532d77bc43e1d3dafe2064739c1aa89b
Thomas Beirne <thomas@buildingconnected.com>
2019-02-26 18:51:40 -0800

    Broken interval

--- index.jsx
+++ index.jsx
@@ -1,54 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-import { Root } from './root'
-import jsonXhr from './json-xhr'
-
-function useFetch(url) {
-	const [result, setResult] = useState()
+function IntervalExample() {
+	const [timeElapsed, setTimeElapsed] = useState(0)
 	useEffect(() => {
-		let xhrInFlight = jsonXhr({ method: 'GET', url }).then(data => {
-			setResult(data)
-			xhrInFlight = null
-		})
-		return () => {
-			if (xhrInFlight) {
-				xhrInFlight.cancel()
-			}
-		}
-	}, [url])
-	return result
+		setInterval(() => {
+			setTimeElapsed(timeElapsed + 1)
+		}, 1000)
+	}, [])
+
+	return <div>Time elapsed in seconds: {timeElapsed}</div>
 }
 
-function OnlineUsers() {
-	const users = useFetch('/online-users')
-	const [selectedUserId, setSelectedUserId] = useState()
-
-	return users === undefined ? (
-		'Loading...'
-	) : (
-		<Root>
-			<ul>
-				{users.map(({ id, name }) => (
-					<li key={id} onClick={() => setSelectedUserId(id)}>
-						{name}
-					</li>
-				))}
-			</ul>
-			{selectedUserId ? <User id={selectedUserId} /> : null}
-		</Root>
-	)
-}
-
-function User({ id, name }) {
-	const userDetail = useFetch(`/users/${id}`)
-
-	return (
-		<div>
-			<h2>{name}</h2>
-			<p>{userDetail ? userDetail.summary : 'Loading...'}</p>
-		</div>
-	)
-}
-
-ReactDOM.render(<OnlineUsers />, document.querySelector('#app'))
+ReactDOM.render(<IntervalExample />, document.querySelector('#app'))
