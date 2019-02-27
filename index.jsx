3a96c3622ae70929661873a058e749158d92a71f
Thomas Beirne <thomas@buildingconnected.com>
2019-02-26 19:39:26 -0800

    Make it work

--- index.jsx
+++ index.jsx
@@ -1,13 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
+
+// Courtesy of Dan Abramov
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+function useInterval(callback, delay) {
+	const savedCallback = useRef()
+
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback
+	})
+
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			savedCallback.current()
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay)
+			return () => clearInterval(id)
+		}
+	}, [delay])
+}
 
 function IntervalExample() {
 	const [timeElapsed, setTimeElapsed] = useState(0)
-	useEffect(() => {
-		setInterval(() => {
-			setTimeElapsed(timeElapsed + 1)
-		}, 1000)
-	}, [])
+	useInterval(() => {
+		setTimeElapsed(timeElapsed + 1)
+	}, 1000)
 
 	return <div>Time elapsed in seconds: {timeElapsed}</div>
 }
